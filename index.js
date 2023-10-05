907c9f0a4b93640c9f42eaf0f458a0d9f0bbb361
Saiishago <mphagosk@gmail.com>
2023-10-05 11:08:48 -0700

    created the correct Shape classes to add to the shapes.js file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const inquirer = require('inquirer');
 const Shapes = require('./lib/shapes.js');
-const Colors = require('./lib/colors.js');
-const Fonts = require('./lib/fonts.js');
+
+const shapes = new Shapes();
+const colors = new Colors();
+const fonts = new Fonts();
+
+shapes.run();
+
+colors.run();
+
+fonts.run();
