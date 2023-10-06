898250b9369359fd42e1cfa07c092df7eac820d8
Saiishago <mphagosk@gmail.com>
2023-10-05 19:47:43 -0700

    Fixed a minor /> error with the .svg files and adjusted the x and y-axis, generated three svg's and added them to my file. Updated my README.md file, it just needs the relevant links before completion

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # SVG_Logo_Maker
+
+## Table of Contents
+- [Description](#description)
+- [Installation Instructions](#installation-instructions) 
+- [Links](#links)
+- [Acknowledgements](#acknowledgements)
+- [Author](#author)
+
+## Description
+This week we were tasked with making and saving an SVG Logo Generator that would run in the command-line.
+
+## Installation Instructions
+i. Clone my SVG_Logo_Maker repository
+[SVG_Logo_Maker by Salome](https://github.com/Saiishago/SVG_Logo_Maker/)
+
+ii. Make sure you have the latest versions of *Node.js*, *Jest* and *Inquirer* installed.
+
+iii. Once cloned and opened in your VS Code, open the intergrated terminal.
+
+iv. Run node index or node index.js to start the process.
+
+v. Done!
+
+NB!! make sure you save the generated svg under another name before making another one, because the new svg will overide the old svg!
+
+
+## Links
+
+### Walkthrough Link
+
+### Github Link
+
+### Screenshots
+#### SVG Image
+![SVG Logo Sample](<svg_examples/SVG Logo Sample.png>)
+
+#### Terminal Questions & Jest Test
+![Terminal Questions and Jest Test](<svg_examples/Terminal Qs and Jest Test.png>)
+
+## Acknowledgements
+YouTube Tutorials by:
+Sanjay Chopra and Thomas Calle
+
+My TA's:
+Brooke Love and Diego Marrs
+
+## Author
+Salome Mphago
+
+© 2023 Salome K Mphago. All Rights Reserved.
--- index.js
+++ index.js
@@ -1,12 +1,110 @@
 const inquirer = require('inquirer');
-const Shapes = require('./lib/shapes.js');
+const fs = require('fs');
+const {Triangle, Circle, Square} = require('./lib/shapes.js');
 
-const shapes = new Shapes();
-const colors = new Colors();
-const fonts = new Fonts();
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "TEXT: Enter up to (3) Characters:",
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "TEXT COLOR: Enter a color keyword or HEX code:",
+    },
+    {
+        type: "input",
+        name: "shape",
+        message: "SHAPE COLOR: Enter a color keyword or HEX code:",
+    },
+    {
+        type: "list",
+        name: "pixel-image",
+        message: "Choose an image you would like?",
+        choices: ["Triangle", "Circle", "Square"],
+    },
+];
 
-shapes.run();
+class SVG{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
 
-colors.run();
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="200" height="100">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="100" y="70" font-size="50" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
 
-fonts.run();
+    }
+    
+};
+
+function writeToFile(fileName, data) {
+	console.log("Writing [" + data + "] to file [" + fileName + "]")
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Welldone, now you have svglogo.svg!");
+    });
+};
+
+async function init() {
+    console.log("Let's go");
+	var svgString = "";
+	var svgFile = "SVGlogo.svg";
+
+    const answers = await inquirer.prompt(questions);
+    var yourText = "";
+    if (answers.text.length > 0 && answers.text.length < 4) {
+        yourText = answers.text;
+    } else {
+        console.log("Wait wait! Please enter JUST three letters/characters, not less.");
+        return;
+    }
+    console.log("Your text: [" + yourText + "]");
+
+    yourTextColor = answers["text-color"];
+    console.log("Your text color: [" + yourTextColor + "]");
+
+    yourShapeColor = answers.shape;
+    console.log("Your shape color: [" + yourShapeColor + "]");
+
+    yourShapeType = answers["pixel-image"];
+    console.log("Your entered shape = [" + yourShapeType + "]");
+
+    let yourShape;
+    if (yourShapeType === "Triangle" || yourShapeType === "triangle") {
+        yourShape = new Triangle();
+		console.log("You selected a Triangle!");
+    }
+    else if (yourShapeType === "Circle" || yourShapeType === "circle") {
+        yourShape = new Circle();
+		console.log("You selected a Circle!");
+    }
+    else if (yourShapeType === "Square" || yourShapeType === "square") {
+        yourShape = new Square();
+		console.log("You selected a Square!");
+    }
+    else {
+        console.log("No, choose from the list");
+    }
+    yourShape.setColor(yourShapeColor);
+
+    var svg = new SVG();
+    svg.setTextElement(yourText, yourTextColor);
+    svg.setShapeElement(yourShape);
+    svgString = svg.render();
+
+    console.log("Showing:\n" + svgString);
+    console.log("Shape, done!");
+    console.log("Processing, wait...");
+    writeToFile(svgFile, svgString);
+}
+init()
